c1d0466c86a4939402dfbcdf31f57199effdf5d0
itspoonam04 <sonampoonam010@gmail.com>
2026-05-19 12:20:07 +0530

    deployment setup

--- artgallery/settings.py
+++ artgallery/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rv461$3#61ju7@y2seoi5p8gfgbqitc1#l(4(*!#kauvom6gc8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'artgallery.urls'
@@ -127,6 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 import os
 from pathlib import Path
 
