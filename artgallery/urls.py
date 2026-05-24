e5bbaa3f634a89377d04b311efaea35f21d13b48
itspoonam04 <sonampoonam010@gmail.com>
2026-05-24 14:50:49 +0530

    added login system

--- artgallery/urls.py
+++ artgallery/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,6 +28,8 @@ urlpatterns = [
     path('cart/', include('cart.urls')),   # ← ADD THIS
     path('wishlist/', include('wishlist.urls')),
     path('orders/', include('orders.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
